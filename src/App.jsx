fe97368f7f0cd04e9ffdbc6997a66b7d5033283a
Nicholas Browning <nicholas.browning@localsearch.com.au>
2025-02-28 11:42:16 +1000

    added edited entire css file

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,11 @@ import './App.css'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello World!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello World!
+      </h1>
+    </div>
   )
 }
 
